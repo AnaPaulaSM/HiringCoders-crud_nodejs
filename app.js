e74634ac8093a49640bee8a2ca77ad4b74122a51
Ana Paula Dos Santos Marianni <ana.santos@somosphi.com>
2022-06-03 15:32:53 -0300

    Corrigido chaves de fechamento e implementado criar, selecionar/atualizar e remover usuario

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const http = require('http');
 const url = require('url');
 const queryString = require('query-string');
+const fs = require('fs');
 
 //Definicao de endereco / URL
 const hostname = '127.0.0.1'; //LOCALHOST
@@ -14,7 +15,42 @@ const server = http.createServer((req, res) => {
   const urlparse = url.parse(req.url, true);
   //Receber as informacoes do usuario
   const params = queryString.parse(url.parse(req.url, true).search);
-            
+      
+  //Criar um usuario - Atualizar um usuario
+
+  if(urlparse.pathname == '/criar-usuario'){
+
+      //Salvar as informacoes
+      fs.writeFile('users/' + params.id + '.txt', JSON.stringify(params), function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+
+        resposta = 'Usuario criado com sucesso!'
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end(resposta);
+      });
+
+ 
+    } //selecionar um usuario
+    else if(urlparse.pathname == '/selecionar-usuario'){
+      fs.readFile('users/'+ params.id + '.txt', function(err, data) {
+      
+      resposta = data;
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end(resposta);  
+      });
+    }
+
+    //Remover um usuario
+    else if(urlparse.pathname == '/remover-usuario'){
+      fs.unlink('users/'+ params.id + '.txt', function (err) {
+        //console.log('File deleted!');
+
+        resposta = err ? 'Usuario nao encontrado!' : 'Usuario removido!';
+        
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/plain');
         res.end(resposta);
