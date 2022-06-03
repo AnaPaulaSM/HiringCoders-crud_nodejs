b81e953353333c14881efea35e63669dd82f9553
Ana P dos Santos <70275763+AnaPaulaSM@users.noreply.github.com>
2022-06-03 15:11:10 -0300

    Implementando a regra de negócio

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 //Importando biblioteca
 const http = require('http');
 const url = require('url');
+const queryString = require('query-string');
 
 //Definicao de endereco / URL
 const hostname = '127.0.0.1'; //LOCALHOST
 const port = 3001;
 
 //Implementacao da regra de negocio (logica de programaçao)
+const server = http.createServer((req, res) => {
+
+  let resposta;
+  const urlparse = url.parse(req.url, true);
+  //Receber as informacoes do usuario
+  const params = queryString.parse(url.parse(req.url, true).search);
+            
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end(resposta);
+      });
+    }
+
+});
 
 //Execução
 server.listen(port, hostname, () => {
